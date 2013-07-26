30505d1f065e3c0fef2c3d4c43cb11b83c68c375
Thanabodee <thanabodee@osdev.co.th>
2013-07-27 00:28:44 +0700

    add view rendering

--- monian/urls.py
+++ monian/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'monian.views.home', name='home'),
+    url(r'^$', 'photo.views.index'),
     # url(r'^monian/', include('monian.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
